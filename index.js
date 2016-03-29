25a078606f10700d359d0f8acc54c776f671cbc2
bdevore17 <bdevore17@gmail.com>
2016-03-29 04:30:30 -0400

    commented out live query

--- index.js
+++ index.js
@@ -24,10 +24,10 @@ var api = new ParseServer({
     "oZiys2BeENO9dUBT/HjRzj4qbGE7P5+bdn6vFXUT",
     "roomadillo",
     {directAccess: true}
-  ),
-  liveQuery: {
-    classNames: ["Roommate", "Match"] // List of classes to support for query subscriptions
-  }
+  )//,
+  // liveQuery: {
+  //   classNames: ["Roommate", "Match"] // List of classes to support for query subscriptions
+  // }
 });
 // Client-keys like the javascript key or the .NET key are not necessary with parse-server
 // If you wish you require them, you can set them as options in the initialization above:
