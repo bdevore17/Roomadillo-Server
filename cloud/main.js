b625b785eb798f2395a1ab05923911d9c4828f01
bdevore17 <bdevore17@gmail.com>
2016-03-30 17:03:04 -0400

    roommate beforeSave

--- cloud/main.js
+++ cloud/main.js
@@ -2,3 +2,10 @@
 Parse.Cloud.define('hello', function(req, res) {
   res.success('Hi');
 });
+
+Parse.Cloud.beforeSave(Parse.Object.extend("Roommate"), function(request, response) {
+	if(request.object.get("viewed") == null) {
+		request.object.set("viewed",[]);
+	}
+	response.success();
+});
