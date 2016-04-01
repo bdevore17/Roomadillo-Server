e8043069416e0452a2a04a4b7081518bbf88d5b5
bdevore17 <bdevore17@gmail.com>
2016-04-01 17:30:16 -0400

    changed before to after

--- cloud/main.js
+++ cloud/main.js
@@ -3,7 +3,7 @@ Parse.Cloud.define('hello', function(req, res) {
   res.success('Hi');
 });
 
-Parse.Cloud.beforeSave(Parse.Object.extend("Roommate"), function(request, response) {
+Parse.Cloud.afterSave(Parse.Object.extend("Roommate"), function(request, response) {
 	if(request.object.get("viewed") == null) {
 		request.object.set("viewed",[request.object.id]);
 	}
