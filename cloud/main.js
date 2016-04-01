d61f47ba67f2edf4659f75c5576edbefad64e420
bdevore17 <bdevore17@gmail.com>
2016-04-01 17:43:48 -0400

    undo changes to id saving

--- cloud/main.js
+++ cloud/main.js
@@ -3,9 +3,9 @@ Parse.Cloud.define('hello', function(req, res) {
   res.success('Hi');
 });
 
-Parse.Cloud.afterSave(Parse.Object.extend("Roommate"), function(request, response) {
+Parse.Cloud.beforeSave(Parse.Object.extend("Roommate"), function(request, response) {
 	if(request.object.get("viewed") == null) {
-		request.object.set("viewed",[request.object.id]);
+		request.object.set("viewed",[]);
 	}
 	response.success();
 });
